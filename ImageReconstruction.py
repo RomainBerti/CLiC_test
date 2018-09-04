4a4b12e421d9ef3157d8ac45041eb879a9b09014
Romain <romain.berti@gmail.com>
2018-09-03 22:48:36 -0400

    code cleaned

--- ImageReconstruction.py
+++ ImageReconstruction.py
@@ -1,120 +1,49 @@
-from __future__ import print_function
 import os
 from PIL import Image
-# import PIL
-import os
-from PIL import Image, ImageFilter
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
-import matplotlib.image as mpimg
-import matplotlib
-from matplotlib import pyplot
-import scipy
+import cv2 as cv
 
 
 IMAGE_FOLDERPATH = './Q1/images/'
-NB_ROW = 3
-NB_COL = 3
+NB_ROW = 9
+NB_COL = 9
 
-list_images = [f for f in os.listdir(IMAGE_FOLDERPATH) if not f.startswith('.')]
+# list all the files in IMAGE_FOLDERPATH without hidden files
+list_images = [image_file for image_file in os.listdir(IMAGE_FOLDERPATH) if not image_file.startswith('.')]
 list_images.sort()
 
-image_finale = []
-it_image = 1
-amp_max = 0
-amp_min = 0
-
+# initialization
+amplitude_max = 0
 # find the maximum over the batch of images
-for file in list_images:
-    im = Image.open(IMAGE_FOLDERPATH + file)
-    X = np.array(im)
-    if np.amax(X) > amp_max:
-        amp_max = np.amax(X)
+for filename in list_images:
+    image = Image.open(IMAGE_FOLDERPATH + filename)
+    image_array = np.array(image)
 
-# print(amp_max) = 1560
-
-# img = cv2.imread(IMAGE_FOLDERPATH + list_images[1])
-# # Display Image
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-
-#overlap is 307
-for row in range(1,9):
-    for col in range(1, 9):
-        # im = mpimg.imread(IMAGE_FOLDERPATH + list_images[it_image])
-        # plt.imshow(im)
-        #
-        # image_finale[1+1024*(col-1):1024*col][ 1024+1014*(row-1) : 1024*row] = numpy.array(im)
-        # it_image +=1
-        continue
-
-
-
-# # # # X = np.random.random((100, 100)) # sample 2D array
-# im = Image.open(IMAGE_FOLDERPATH + list_images[1])
-# im = (im).astype('uint8')
-#
-# plt.imshow(im, cmap='jet')
-# plt.show()
-# print(im.size)
-# print(im.mode)
-# im = im.convert('RGB')
-#
-# print(im.mode)
-# # X = np.fliplr(np.flipud(np.array(im)))
-# im = im.resize((128, 128), Image.ANTIALIAS)
-# print(im.size)
-# # X = np.array(im)
-#
-# plt.imshow(im, cmap='jet')
-# plt.show()
-
-# im = Image.open(IMAGE_FOLDERPATH + list_images[1])
-# img = np.array(im)
-# res = cv2.resize(img, dsize=(128, 128), interpolation=cv2.INTER_LANCZOS4)
-# plt.imshow(res, cmap='gray')
-# plt.show()
-
-files = list_images
+    if np.amax(image_array) > amplitude_max:
+        amplitude_max = np.amax(image_array)
 
 result = Image.new("RGB", (900, 900))
-X_data = []
-final = []
-tableau = np.zeros((900,900))
 
+# I choose a final image size of 900x900 composed of 100x100 images
+reconstructed_image = np.zeros((900, 900))
+overlap = 24
+small_image_size = 100
+image_increment = round(small_image_size * overlap / 1024)
 
-#for index, file in enumerate(files):
-for i in range(0,81):
-    path = os.path.expanduser(IMAGE_FOLDERPATH + list_images[i])
-    img = Image.open(path)
-    img = np.array(img)/amp_max*255
-    X_data = cv2.resize(img, dsize=(100, 100), interpolation=cv2.INTER_LANCZOS4)
-    # I suppose that the camera goes from left to right and up to down (not in a Z way)
-    x = i // 9 * 98
-    y = i % 9 * 98
-    # print(x,y)
-    # print(tableau[x:x+100,y:y+100].shape)
-    # print(X_data.shape)
-    tableau[x:x+100,y:y+100] = X_data
-    # plt.imshow(X_data, cmap='gray')
-    # plt.show()
-    # final = np.vstack((tableau, X_data))
-  # res.thumbnail((400, 400), Image.ANTIALIAS)
-  # x = index // 2 * 400
-  # y = index % 2 * 400
-  # w, h = 128, 128
-  # print('pos {0},{1} size {2},{3}'.format(x, y, w, h))
-  # result.paste(img, (x, y, x + w, y + h))
+for it_image, filename in enumerate(list_images):
+    path = os.path.expanduser(IMAGE_FOLDERPATH + filename)
+    image = Image.open(path)
+    # normalize image on 8bits (0-255)
+    image = np.array(image)/amplitude_max * 255
+    # resize image
+    small_image = cv.resize(image, dsize=(small_image_size, small_image_size), interpolation=cv.INTER_LANCZOS4)
+    x = it_image // NB_COL * (small_image_size - image_increment)  # floor division to go through the col
+    y = it_image % NB_ROW * (small_image_size - image_increment)  # y is modulo of the width of the full image
+    reconstructed_image[x:x+small_image_size, y:y+small_image_size] = small_image
 
-# result.save(os.path.expanduser('./image.jpg'))
-# print(X_data)
-# print(tableau.shape)
-plt.imshow(tableau, cmap='seismic')
+plt.imshow(reconstructed_image, cmap='seismic')
 plt.show()
-cv2.imwrite('./test.png', tableau)
+cv.imwrite('./Q1_reconstructed_image.png', reconstructed_image)
 
 
--- peakDetect.py
+++ peakDetect.py
@@ -1,50 +1,41 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
 import os
 from scipy import ndimage
 
 
 IMAGE_FOLDERPATH = './Q2/images/'
-
 list_images = [image_file for image_file in os.listdir(IMAGE_FOLDERPATH) if not image_file.startswith('.')]
 list_images.sort()
 
-for i, image in enumerate(list_images):
+RESULT_DIRECTORY = './Q2/results/'
+if not os.path.exists(RESULT_DIRECTORY):
+    os.makedirs(RESULT_DIRECTORY)
+
+GREEN_RGB = (0, 255, 0)
+h = 5  # Parameter regulating filter strength
+h_for_color_components = 5  # The same as h but for color components.
+template_window_size = 5  # Size in pixels of the template patch that is used to compute weights.
+search_window_size = 15  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+threshold = 240  # threshold value.
+max_value = 255  # maximum value to use with the THRESH_BINARY and THRESH_BINARY_INV thresholding types.
+
+for it_image, filename in enumerate(list_images):
 
     # noise reduction
-    img = cv.imread(IMAGE_FOLDERPATH + image)
-    img_to_display = (img / np.amax(img) * 255) .astype(np.uint8)
+    image = cv.imread(IMAGE_FOLDERPATH + filename)
+    denoise_image = cv.fastNlMeansDenoisingColored(image, None, h, h_for_color_components,
+                                                   template_window_size, search_window_size)
+    denoise_norm_image = (denoise_image/np.amax(denoise_image)*255).astype(np.uint8)
+    bw_image = cv.cvtColor(denoise_image, cv.COLOR_BGR2GRAY)
 
-    denoise_img = cv.fastNlMeansDenoisingColored(img, None, 5, 5, 5, 15)
-    denoise_norm_img = (denoise_img/np.amax(denoise_img)*255).astype(np.uint8)
-    # plt.imshow(denoise_norm_img, cmap='gray')
-    # plt.show()
-
-
-    bw_img = cv.cvtColor(denoise_img, cv.COLOR_BGR2GRAY)
-    max_val = np.amax(bw_img)
-    norm_img = (bw_img/max_val*255).astype(np.uint8)
-
-    # plt.imshow(denoise_img, cmap='gray')
-    # plt.show()
-
-    ret, thresh1 = cv.threshold(denoise_norm_img, 240, 250, cv.THRESH_BINARY)
-    value = ndimage.measurements.center_of_mass(thresh1)
+    ret, threshold_image = cv.threshold(denoise_norm_image, threshold, max_value, cv.THRESH_BINARY)
+    value = ndimage.measurements.center_of_mass(threshold_image)
     x_center = int(round(value[1]))
     y_center = int(round(value[0]))
-    # print(x_center, y_center)
 
-    # plt.imshow(thresh1, cmap='gray')
-    # plt.show()
-
-
-    # Draw a diagonal blue line with thickness of 5 px
-    image_with_mark = cv.line(thresh1, (x_center, y_center), (x_center, y_center), (0, 255, 0), 1)
-    # plt.imshow(image_with_mark, cmap='gray')
-    # plt.show()
-    cv.imwrite('./Q2/images/image_' + str(i) + 'processed.jpg', image_with_mark)
-
-
-
-
+    # Draw a green point with thickness of 1 px
+    marked_image = cv.line(threshold_image, (x_center, y_center), (x_center, y_center), GREEN_RGB, 1)
+    # Write result
+    cv.imwrite(RESULT_DIRECTORY + 'image_' + str(it_image) + '_processed.jpg', marked_image)
